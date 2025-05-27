7760ebfbff1512355eec25cc65c584a70764f756
doumao <shenren1993@gmail.com>
2025-05-27 17:55:27 +0800

    监听排除gen

--- watch/watch_mosdns.sh
+++ watch/watch_mosdns.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 # 延迟 60 秒
 sleep 60
+
 # 需要监听的目录
 CONFIG_DIR="/mssb/mosdns"
 
-# 使用 inotifywait 监听特定文件类型的变动
+# 使用 inotifywait 监听特定文件类型的变动，排除 gen 目录
 while true; do
-  inotifywait -e modify,create,delete -r $CONFIG_DIR --include '.*\.(txt|yaml|json|srs|yml)$'
+  inotifywait -e modify,create,delete -r "$CONFIG_DIR" \
+    --include '.*\.(txt|yaml|json|srs|yml)$' \
+    --exclude '^/mssb/mosdns/gen/.*'
+
   echo "MosDNS 配置文件发生变化，重启 MosDNS 服务..."
 
   # 通过 supervisorctl 重启 MosDNS 服务
